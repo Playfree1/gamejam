b8be1edac9c7ce22c82c433bcde88b293a443809
Playfree1 <137008415+Playfree1@users.noreply.github.com>
2025-05-02 14:00:45 +0300

    sync

--- Assets/scripts/bullet/bulletBoss1.cs
+++ Assets/scripts/bullet/bulletBoss1.cs
@@ -3,13 +3,19 @@ using UnityEngine;
 
 public class bulletBoss1 : MonoBehaviour
 {
-    private GameObject player;
+    public Quaternion _rotate;
     IEnumerator Start()
     {
-
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.01f);
+        Instantiate(transform.parent.gameObject, transform.GetChild(0).position, _rotate, transform.parent);
+        yield return new WaitForSeconds(0.3f);
         Destroy(transform.parent.gameObject);
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
 
+            Destroy(transform.root.gameObject);
+        
+    }
 
 }
--- Assets/scripts/enemy/boss1/shotBoss1.cs
+++ Assets/scripts/enemy/boss1/shotBoss1.cs
@@ -15,7 +15,7 @@ public class shotBoss1 : MonoBehaviour
 
     void Update()
     {
-        if (Vector2.Distance(transform.position, player.transform.position) < 4f)
+        if (Vector2.Distance(transform.position, player.transform.position) < 20f)
         {
             if (count > 1)
             {
@@ -30,16 +30,10 @@ public class shotBoss1 : MonoBehaviour
     IEnumerator Shot()
     {
         Vector3 direction = player.transform.position - transform.position;
-        Quaternion rotate = Quaternion.Euler(0, 0, -Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg);
-        player = GameObject.FindGameObjectWithTag("Player");
-        Vector2 vector2 = player.transform.position - transform.position;
-        float distance = vector2.magnitude;
-        vector2.Normalize();
-        Quaternion _rotate = Quaternion.Euler(0, 0, -Mathf.Atan2(vector2.x, vector2.y) * Mathf.Rad2Deg - 90);
-        Vector2 _scale = new Vector2(distance * 1.4f, 0.1f);
+        Quaternion rotate = Quaternion.Euler(0, 0, -Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + 90);
         yield return new WaitForSeconds(0.15f);
         GameObject obj = Instantiate(bullet, transform.position, rotate);
-        obj.transform.rotation = _rotate;
-        obj.transform.localScale = _scale;
+        obj.GetComponent<bulletBoss1>()._rotate = rotate;
+
     } 
 }
